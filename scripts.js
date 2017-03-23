a57d9b1137f99769b959ae07d99b63a0eb88827c
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-03-22 21:40:12 -0500

    STUCKKKKKKK

--- scripts.js
+++ scripts.js
@@ -1,50 +1,59 @@
 // alert("hello world");
-console.log("Hello world");
+console.log("Open");
+
 var productOutput = document.getElementById("data-output");
+var selector = document.getElementById("seasons");
 
-function productsLoop(product){
-  var newProduct = "";
-  var currentProduct = "";
-  for(var i=0;i<product.products.length;i++){
-    currentProduct = product.products[i];
-    newProduct += `<div class="col-sm-6 col-md-4">`;
-    newProduct += `<h3>${currentProduct.name}</h3>`
-    newProduct += `<div class="thumbnail">`;
-    newProduct += `<img src="..." alt="...">`;
-    newProduct += `<div class="caption">`;
-    newProduct += `<p>${currentProduct.name}</p>`;
-    newProduct += `<p>${currentProduct.price}</p>`;
-    newProduct += `</div></div></div>`;
-    for(var y=0;y<product.categories.length;y++){
-      currentCategory = product.categories[y];
-      console.log(product.categories[y]);
-    }
-  }
-  productOutput.innerHTML = newProduct;
-}
-
-
-
-
-function categoriesLoaded(){
-  var categoriesData = JSON.parse(this.responseText)
-  console.log(categoriesData);
-}
-
-function productsLoaded(){
-  var productData = JSON.parse(this.responseText)
-  console.log(productData);
-  productsLoop(productData);
-}
 
 var productsRequest = new XMLHttpRequest();
 productsRequest.addEventListener("load", productsLoaded);
 productsRequest.open("GET", "products.json");
 productsRequest.send();
-console.log("Running...");
 
-var categoriesRequest = new XMLHttpRequest();
-categoriesRequest.addEventListener("load", categoriesLoaded);
-categoriesRequest.open("GET", "categories.json");
-categoriesRequest.send();
-console.log("Running...");
+function failedExecution() {
+    alert("Error loading page. Please refresh.")
+};
+
+function productsLoaded(){
+  var productData = JSON.parse(this.responseText);
+  loadCategories(productData);
+}
+
+function loadCategories(prod){
+  var categoriesRequest = new XMLHttpRequest();
+  categoriesRequest.addEventListener("load", categoriesLoaded);
+  categoriesRequest.open("GET", "categories.json");
+  categoriesRequest.send();
+
+  function categoriesLoaded(){
+    var categoriesData = JSON.parse(this.responseText)
+    printToDom(productData, categoriesData);
+  }
+}
+
+// function categoriesLoop(){
+//   var newCategory = "";
+//   for(var i=0;i<.categories;i++){
+//
+//   }
+// }
+
+function printToDom(prod,cat){
+    var newProduct = "";
+    var currentProduct = "";
+    for(var i=0;i<prod.products.length;i++){
+      currentProduct = prod.products[i];
+      newProduct += `<div class="col-sm-6 col-md-4">`;
+      newProduct += `<h3>${currentProduct.name}</h3>`;
+      newProduct += `<div class="thumbnail">`;
+      newProduct += `<div class="caption">`;
+      newProduct += `<p>${currentProduct.name}</p>`;
+      newProduct += `<p>${currentProduct.price}</p>`;
+      newProduct += `</div></div></div>`;
+      // Categories
+
+      }
+    productOutput.innerHTML = newProduct;
+}
+
+console.log("close");
