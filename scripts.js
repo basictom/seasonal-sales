31e75b6b2dc605c049463be65fd3e240e659135c
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-03-23 20:10:41 -0500

    completed assignment

--- scripts.js
+++ scripts.js
@@ -4,56 +4,76 @@ console.log("Open");
 var productOutput = document.getElementById("data-output");
 var selector = document.getElementById("seasons");
 
+function failedExecution() {
+    alert("Error loading page. Please refresh.")
+};
+
+var productData = [];
+var categoriesData = [];
+
+function categoriesLoaded(){
+  categoriesData = JSON.parse(this.responseText).categories;
+  // console.log(categoriesData);
+}
+
+function productsLoaded(){
+  productData = JSON.parse(this.responseText).products;
+  handleData();
+}
+
+
+function printToDom(disc){
+    var newProduct = "";
+    // console.log("newproduct", newProduct);
+    for(var i=0;i<productData.length;i++){
+      // currentProduct = discount.products[i];
+      newProduct += `<div class="col-sm-6 col-md-4">`;
+      newProduct += `<h3>${productData[i].name}</h3>`;
+      newProduct += `<div class="thumbnail">`;
+      newProduct += `<div class="caption">`;
+      newProduct += `<p>${productData[i].name}</p>`;
+      if(disc === productData[i].season_discount){
+      newProduct += `<p>${productData[i].season_price.toFixed(2)}</p>`;
+      }else{
+      newProduct += `<p>${productData[i].price}</p>`;
+      }
+      newProduct += `</div></div></div>`;
+      }
+    productOutput.innerHTML = newProduct;
+}
+
+function handleData(){
+  productData.forEach(function(product){
+    for(var i=0;i<categoriesData.length;i++){
+      if(product.category_id === categoriesData[i].id){
+        product["name"] = categoriesData[i].name;
+        product["discount"] = categoriesData[i].discount;
+        product["season_discount"] = categoriesData[i].season_discount;
+        product["season_price"] = product.price -  (product.price * categoriesData[i].discount);
+      }
+    }
+  })
+  console.log(categoriesData);
+  console.log(productData);
+  printToDom('null');
+}
+
+selector.addEventListener("change", function(e){
+  var selectedSeason = event.target.value;
+  printToDom(selectedSeason);
+})
+
+
+var categoriesRequest = new XMLHttpRequest();
+categoriesRequest.addEventListener("load", categoriesLoaded);
+categoriesRequest.open("GET", "categories.json");
+categoriesRequest.send();
 
 var productsRequest = new XMLHttpRequest();
 productsRequest.addEventListener("load", productsLoaded);
 productsRequest.open("GET", "products.json");
 productsRequest.send();
 
-function failedExecution() {
-    alert("Error loading page. Please refresh.")
-};
 
-function productsLoaded(){
-  var productData = JSON.parse(this.responseText);
-  loadCategories(productData);
-}
-
-function loadCategories(prod){
-  var categoriesRequest = new XMLHttpRequest();
-  categoriesRequest.addEventListener("load", categoriesLoaded);
-  categoriesRequest.open("GET", "categories.json");
-  categoriesRequest.send();
-
-  function categoriesLoaded(){
-    var categoriesData = JSON.parse(this.responseText)
-    printToDom(productData, categoriesData);
-  }
-}
-
-// function categoriesLoop(){
-//   var newCategory = "";
-//   for(var i=0;i<.categories;i++){
-//
-//   }
-// }
-
-function printToDom(prod,cat){
-    var newProduct = "";
-    var currentProduct = "";
-    for(var i=0;i<prod.products.length;i++){
-      currentProduct = prod.products[i];
-      newProduct += `<div class="col-sm-6 col-md-4">`;
-      newProduct += `<h3>${currentProduct.name}</h3>`;
-      newProduct += `<div class="thumbnail">`;
-      newProduct += `<div class="caption">`;
-      newProduct += `<p>${currentProduct.name}</p>`;
-      newProduct += `<p>${currentProduct.price}</p>`;
-      newProduct += `</div></div></div>`;
-      // Categories
-
-      }
-    productOutput.innerHTML = newProduct;
-}
 
 console.log("close");
